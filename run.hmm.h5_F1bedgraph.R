404c06885c6df7f1fd74d4a1cc30d55160ab720b
shaopei <sc2457@cornell.edu>
2019-05-23 11:49:55 -0400

    add chrX

--- run.hmm.h5_F1bedgraph.R
+++ run.hmm.h5_F1bedgraph.R
@@ -32,7 +32,7 @@ ref.params.path = args[5]
 
 
 chrom=c("chr1",  "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17","chr18", "chr19", 
-  "chr2",  "chr3",  "chr4" , "chr5" , "chr6" , "chr7" , "chr8", "chr9")
+  "chr2",  "chr3",  "chr4" , "chr5" , "chr6" , "chr7" , "chr8", "chr9", "chrX")
 #
 # prepare data
 #
