0eb648282422992f23a92ca3cb0e3b783f796505
shaopei <sc2457@cornell.edu>
2019-08-14 17:40:30 -0400

    use correct input for KS test in R

--- F1_Pausing_whole_dREG.sh
+++ F1_Pausing_whole_dREG.sh
@@ -5,7 +5,7 @@ studyBed=dREG
 for Head in HT KD SK
 do
 #  bedtools coverage -a <(zcat Browser/${Head}_all.dREG.peak.score.bed.gz| awk 'BEGIN{OFS="\t"} {print $0, ".", "+"} {print $0, ".", "-"}') -b <(zcat map2ref_1bpbed/${Head}_PB6_*_dedup_R1.mat.bowtie.gz_AMBremoved_sorted_specific.map2ref.1bp.sorted.bed.gz) | awk 'BEGIN{OFS="\t"} ($7 >=5){print $1,$2,$3,$4,$5,$6}' > ${intermediate_file}
-  bedtools coverage -s -a <(bedtools coverage -a <(zcat Browser/${Head}_all.dREG.peak.score.bed.gz| awk 'BEGIN{OFS="\t"} {print $0, ".", "+"} {print $0, ".", "-"}') -b <(zcat map2ref_1bpbed/${Head}_PB6_*_dedup_R1.mat.bowtie.gz_AMBremoved_sorted_specific.map2ref.1bp.sorted.bed.gz) | awk 'BEGIN{OFS="\t"} ($7 >=5){print $1,$2,$3,$4,$5,$6}') -b <(zcat map2ref_1bpbed/${Head}_PB6_*_dedup_R1.pat.bowtie.gz_AMBremoved_sorted_specific.map2ref.1bp.sorted.bed.gz) | awk 'BEGIN{OFS="\t"} ($7 >=5){print $1,$2,$3,$4,$5,$6}'| sort-bed - |uniq > ${Head}_${studyBed}_5mat5pat_uniq.bed &
+  bedtools coverage -s -a <(bedtools coverage -a <(zcat Browser/${Head}_all.dREG.peak.score.bed.gz| awk 'BEGIN{OFS="\t"} {print $0, ".", "+"} {print $0, ".", "-"}') -b <(zcat map2ref_1bpbed/${Head}_PB6_*_dedup_R1.mat.bowtie.gz_AMBremoved_sorted_specific.map2ref.1bp.sorted.bed.gz) | awk 'BEGIN{OFS="\t"} ($7 >=5){print $1,$2,$3,$4,$5,$6}') -b <(zcat map2ref_1bpbed/${Head}_PB6_*_dedup_R1.pat.bowtie.gz_AMBremoved_sorted_specific.map2ref.1bp.sorted.bed.gz) | awk 'BEGIN{OFS="\t"} ($7 >=5){print $1,$2,$3,$4,$5}'| sort-bed - |uniq |awk 'BEGIN{OFS="\t"} {print $0, "+"} {print $0, "-"}'> ${Head}_${studyBed}_5mat5pat_uniq.bed &
 done
 wait
 
@@ -25,7 +25,7 @@ for Head in HT KD SK
 do
   for allele in mat pat
   do
-python Generate_vector_input_for_KStest.py ${Head}_${allele}_temp.bed ${Head}_${studyBed}_5mat5pat_uniq_${allele}.perBase.bed
+python Generate_vector_input_for_KStest.py ${Head}_${allele}_temp.bed ${Head}_${studyBed}_5mat5pat_uniq_${allele}.perBase.bed &
 done
 done
 wait
@@ -33,29 +33,29 @@ wait
 # get p-value for KS test in R
 for Tissue in KD SK HT 
 do
-R --vanilla --slave --args $(pwd) ${Tissue} ${studyBed}_200bp_5mat5pat_uniq < KStest.R &
+R --vanilla --slave --args $(pwd) ${Tissue} ${studyBed}_5mat5pat_uniq < KStest_flexible_length.R &
 done
 
 # HERE!!!
 for Head in HT KD SK
 do
-  echo ${Head}_all.dREG.peak.score.bed.gz
+  #echo ${Head}_all.dREG.peak.score.bed.gz
 zcat Browser/${Head}_all.dREG.peak.score.bed.gz |wc -l
 done
 wait
 for Tissue in HT KD SK
 do
-  cat ${Tissue}_${studyBed}_200bp_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($7 <= 0.05){print $0, $1c$2d$3 }' |wc -l
+  cat ${Tissue}_${studyBed}_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($7 <= 0.05){print $0, $1c$2d$3 }' |wc -l
 done
 for Tissue in HT KD SK
 do
-  cat ${Tissue}_${studyBed}_200bp_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($7 <= 0.15){print $0, $1c$2d$3 }' |wc -l
+  cat ${Tissue}_${studyBed}_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($8 <= 0.1){print $0, $1c$2d$3 }' |wc -l
 done
 
 for Tissue in HT KD SK
 do
-  echo ${Tissue}_${studyBed}_200bp_5mat5pat_uniq_pValue.bed 
-  cat ${Tissue}_${studyBed}_200bp_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($7 <= 0.15){print $0, $1c$2d$3 }' 
+  echo ${Tissue}_${studyBed}_5mat5pat_uniq_pValue.bed 
+  cat ${Tissue}_${studyBed}_5mat5pat_uniq_pValue.bed | awk 'BEGIN{OFS="\t"; c=":"; d="-"} ($7 <= 0.15){print $0, $1c$2d$3 }' 
 done
 
 
---
+++ Generate_vector_input_for_KStest.py
@@ -0,0 +1,67 @@
+import numpy as np
+from math import *
+from sys import argv
+from multiprocessing import Pool
+infp=argv[1]
+outfp=argv[2]
+span=100
+
+sample="_".join(infp.split("_")[0:2])
+data=np.array([l.strip().split("\t") for l in open(infp).readlines()])
+chrom_list= list(set(data[:,0]))
+chrom_list.sort()
+data_dic={}
+for c in chrom_list:
+    data_dic[c] =  data[data[:,0]==c,:]
+
+data=[]
+
+def getVector(data_key):
+    result=[]
+    data=data_dic[data_key]
+    count=1
+    while data.shape[0] > 10:
+        chrom, chromStart, chromEnd = data[0][0:3]
+        #c0 = data[:,0]==chrom
+        c1 = data[:,1]==chromStart
+        c2 = data[:,2]==chromEnd
+        name = chrom+"_"+str(count)
+        inside = np.logical_and(c1,c2)
+        outside = np.invert(inside)
+        subdata = data[inside,:]
+        data = data[outside,:]
+        sub_plus = subdata[subdata[:,5] == "+", :]
+        sub_minus = subdata[subdata[:,5] == "-", :]
+        
+        sp=[]
+        for s in sub_plus:
+            sp = sp + [s[6]]*int(s[7])
+        
+        sm=[]
+        for s in sub_minus:
+            sm = sm + [s[6]]*int(s[7])
+        
+        if len(sm) > 0:
+            result.append("\t".join([chrom, chromStart, chromEnd, name,".", "-", ",".join(sm)]))
+        else:
+            result.append("\t".join([chrom, chromStart, chromEnd, name,".", "-", "0"]))
+        if len(sp) > 0:
+            result.append("\t".join([chrom, chromStart, chromEnd, name,".", "+", ",".join(sp)]))
+        else:
+            result.append("\t".join([chrom, chromStart, chromEnd, name,".", "+", "0"]))
+        count = count + 1
+    return result
+
+
+pool = Pool(processes=25)
+pool_output = pool.map(getVector, chrom_list)
+# pool_output looks like [[t, new_T_list,new_P_list, p_Y_f_list],...]
+pool.close() # no more tasks
+pool.join()
+
+
+
+with open(outfp, "w") as out:
+    for line in pool_output:
+        out.write("\n".join(line))
+        out.write("\n")
