b1c31c3ed78d8a44b8f67a7837517759f12b13db
shaopei <sc2457@cornell.edu>
2019-11-05 16:08:34 -0500

    Allelic bias switch in domains

--- allelic_bias_swtiches.R
+++ allelic_bias_swtiches.R
@@ -1,5 +1,6 @@
 setwd("~/Box Sync/Danko_lab_work/F1_8Tissues/")
-df=read.table("allelic_bias_swtiches.txt", header = T)
+#df=read.table("allelic_bias_swtiches.txt", header = T)
+df=read.table("allelic_bias_swtiches_SIblocks.txt", header = T)
 for (i in 1:nrow(df)){
   a = fisher.test(matrix(c(df$im2[i],df$im1[i], df$st2[i], df$st1[i]), 2,2))
   df$p.value[i] = a$p.value
