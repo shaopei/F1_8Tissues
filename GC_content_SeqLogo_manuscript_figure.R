34349a274b5bb23497d4df09bf4d946c5b4fc70f
shaopei <sc2457@cornell.edu>
2020-06-02 17:37:10 +0800

    script for GC_content_SeqLogo_manuscript_figure

---
+++ GC_content_SeqLogo_manuscript_figure.R
@@ -0,0 +1,230 @@
+
+library("TmCalculator")
+library(seqLogo)
+setwd("~/Box Sync/Danko_lab_work/F1_8Tissues/Initiation/GC_content")
+
+
+High_Low_GC_excludeCA <- function(df, name, step=5){
+  pdf(paste(name,"_GC_content_excludeCA_step=",step,".pdf", sep = ""))
+  #par(mfrow=c(4,1))
+  #for (step in c(1,5,10,20)){
+  h<- NULL
+  for (i in 1:NROW(df)){
+    a=s2c(as.character(df$HighAlleleSeq[i]))
+    len_a = length(a)
+    d = (len_a-1)/2
+    #CA at a[d:(d+1)]
+    a = c(a[1:(d-1)] ,"N","N",a[(d+2):len_a])
+    #len_a = length(a)
+    temp <- NULL
+    if (step>1){
+      for (j in seq(1,(len_a-step), step)){
+        temp = c(temp, GC(c2s(a[j:(j+step-1)])))
+      }
+      h <- rbind(h,temp)
+    }else{
+      for (j in seq(1,(len_a), step)){
+        temp = c(temp, GC(c2s(c(a[j],"A"))))
+      }
+      h <- rbind(h,temp)
+    }
+  }
+  HighTm = apply(h, 2, FUN = mean, na.rm = TRUE) 
+  #colMeans(m)
+  
+  l<- NULL
+  for (i in 1:NROW(df)){
+    a=s2c(as.character(df$LowAlleleSeq[i]))
+    len_a = length(a)
+    d = (len_a-1)/2
+    #CA at a[d:(d+1)]
+    a = c(a[1:(d-1)] ,"N","N",a[(d+2):len_a])
+    #len_a = length(a)
+    temp <- NULL
+    if (step>1){
+      for (j in seq(1,(len_a-step), step)){
+        temp = c(temp, GC(c2s(a[j:(j+step-1)])))
+      }
+      l <- rbind(l,temp)
+    }else{
+      for (j in seq(1,(len_a), step)){
+        temp = c(temp, GC(c2s(c(a[j],"A"))))
+      }
+      l <- rbind(l,temp)
+    }
+  }
+  
+  LowTm = apply(l, 2, FUN = mean, na.rm = TRUE) 
+  show.window = floor(len_a/2)
+  if (step >1){
+    x <- seq(-1*(show.window)+floor(step/2), show.window, step)
+  }else{
+    x <- seq(-1*(show.window), show.window, step)
+  }
+  
+  if (step>1){
+    plot(x[1:length(LowTm)], LowTm, col="blue", xlab="distance to as.maxTSN", 
+         ylab=paste("mean of GC(around x, bin=",step,"bp)",sep=""),
+         #xlim=c(-50,50),
+         main=paste(name, " n=", dim(df)[1], sep=""), 
+         type="o", 
+         ylim=c(min(HighTm,LowTm),max(HighTm,LowTm)), 
+         las=1)
+    points(x[1:length(LowTm)], HighTm,col="red", type="o")
+  } else {
+    plot(x[1:length(LowTm)], LowTm*2, col="blue", xlab="distance to maxTSN", ylab="mean of GC content", 
+         #xlim=c(-50,50),
+         main=name, type="o", ylim=c(min(HighTm*2,LowTm*2),max(HighTm*2,LowTm*2)), las=1)
+    points(x[1:length(LowTm)], HighTm*2,col="red", type="o")
+  }
+  
+  legend("bottomleft", legend=c("High", "Low"),
+         col=c("red", "blue"), bty = "n", lty=1, pch=1)
+  #}
+  dev.off()
+  return (list(x, HighTm, LowTm, show.window))
+}
+
+
+df_process <- function(df, name, step){
+  
+  colnames(df)[16:17] = c("HighAlleleSeq", "LowAlleleSeq")
+  colnames(df)[7:10]=c("masked_p_value","masked_fdr", "unmasked_p_value","unmasked_fdr")
+  colnames(df)[11] = "winP"
+  g9 = df[df$unmasked_fdr> 0.9,]
+  p_value_cut = max(df$unmasked_p_value[df$unmasked_fdr<=0.1])
+  
+  # asTSS driven by multiple base
+  m = df[df$masked_p_value <= p_value_cut & df$unmasked_fdr<=0.1,]
+  # asTSS driven by single base
+  s = df[df$masked_p_value > p_value_cut & df$unmasked_fdr<=0.1,]
+  cat(name, "\n")
+  cat("dim(g9); max(g9$unmasked_p_value); min(g9$unmasked_p_value)", "\n")
+  cat (dim(g9), max(g9$unmasked_p_value), min(g9$unmasked_p_value) )
+  cat ("dim(m), max(m$masked_p_value), min(m$masked_p_value)", "\n")
+  cat (dim(m), max(m$masked_p_value), min(m$masked_p_value), "\n")
+  cat("  dim(s); max(s$masked_p_value); min(s$masked_p_value)", "\n")
+  cat(dim(s), max(s$masked_p_value), min(s$masked_p_value), "\n")
+  
+  
+  g9_gc_e=High_Low_GC_excludeCA(g9, name=paste(organ,"_fdr>0.9_d=",d,sep=""), step=step)
+  m_gc_e=High_Low_GC_excludeCA(m, name=paste(organ,"_m_d=",d,sep=""), step=step)
+  s_gc_e=High_Low_GC_excludeCA(s, name=paste(organ,"_s_d=",d,sep=""), step=step)
+  return(list(g9_gc_e, m_gc_e, s_gc_e))
+}
+
+d=35; step=5
+organ="LV"; 
+LV_df=read.table(paste(organ, "_allReads_TSS_5mat5pat_uniq_maskedVSunmasked_BinomialTest_maxTSNs_+-",d,"_High_LowAlleleSeq.bed", sep=""))
+LV_output = df_process(LV_df, organ, step)
+
+organ="BN"
+BN_df=read.table(paste(organ, "_allReads_TSS_5mat5pat_uniq_maskedVSunmasked_BinomialTest_maxTSNs_+-",d,"_High_LowAlleleSeq.bed", sep=""))
+#name=organ
+
+BN_output = df_process(BN_df, organ, step)
+
+
+# Single base plot
+g9_gc_e = LV_output[[1]]
+m_gc_e = LV_output[[2]]
+s_gc_e = LV_output[[3]]
+
+#pdf(paste(organ,"_deltaGC_exclude_CA_step=",step,"_d=",d,".pdf" ,sep=""), width = 10, height = 10)
+plot(s_gc_e[[1]][1:length(s_gc_e[[2]])], s_gc_e[[2]]-s_gc_e[[3]], type="o", col="pink", 
+     ylim=c(min(s_gc_e[[2]]-s_gc_e[[3]], m_gc_e[[2]]-m_gc_e[[3]]), max(s_gc_e[[2]]-s_gc_e[[3]], m_gc_e[[2]]-m_gc_e[[3]])),
+     main=paste("High Allele - Low Allele, d=",g9_gc_e[[4]] ," bin size=",step,"_exclude maxTSN",sep=""),
+     xlab="distance to maxTSN", ylab="delta GC", pch=6, xlim=c(-d,d))
+#abline(h=0)
+#abline(v=0)
+
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], g9_gc_e[[2]]-g9_gc_e[[3]], type="o", col="gray", pch=6)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(s_gc_e[[1]][1:length(s_gc_e[[2]])], s_gc_e[[2]]-s_gc_e[[3]], type="o", col="pink", pch=6)
+
+
+
+
+g9_gc_e = BN_output[[1]]
+m_gc_e = BN_output[[2]]
+s_gc_e = BN_output[[3]]
+
+
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], g9_gc_e[[2]]-g9_gc_e[[3]], type="o", col="gray", pch=1)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(s_gc_e[[1]][1:length(s_gc_e[[2]])], s_gc_e[[2]]-s_gc_e[[3]], type="o", col="pink", pch=1)
+
+
+a_g9_gc_e = ((BN_output[[1]][[2]]- BN_output[[1]][[3]]) + (LV_output[[1]][[2]]- LV_output[[1]][[3]]))/2
+a_m_gc_e =((BN_output[[2]][[2]]- BN_output[[2]][[3]]) + (LV_output[[2]][[2]]- LV_output[[2]][[3]]))/2
+a_s_gc_e = ((BN_output[[3]][[2]]- BN_output[[3]][[3]]) + (LV_output[[3]][[2]]- LV_output[[3]][[3]]))/2
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], a_g9_gc_e, type="o", col="black", pch=19)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(s_gc_e[[1]][1:length(s_gc_e[[2]])], a_s_gc_e, type="o", col="red", pch=19)
+
+legend("bottomleft", legend=c("Single Mean", "Single Brain", "Single Liver", "NS Mean", "NS Brain", "NS Liver"),
+       col=c("red","pink","pink", "black","gray","gray"), bty = "n", lty=1, pch=c(19,1,6,19,1,6))
+
+dev.off()
+
+# Multiple base plot
+
+
+
+g9_gc_e = BN_output[[1]]
+m_gc_e = BN_output[[2]]
+s_gc_e = BN_output[[3]]
+
+#pdf(paste(organ,"_deltaGC_exclude_CA_step=",step,"_d=",d,".pdf" ,sep=""), width = 10, height = 10)
+plot(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="light blue", 
+     #ylim=c(min(m_gc_e[[2]]-m_gc_e[[3]], m_gc_e[[2]]-m_gc_e[[3]]), max(m_gc_e[[2]]-m_gc_e[[3]], m_gc_e[[2]]-m_gc_e[[3]])),
+     ylim=c(-0.9, 0.5),
+     main=paste("High Allele - Low Allele, d=",g9_gc_e[[4]] ," bin size=",step,"_exclude maxTSN",sep=""),
+     xlab="distance to maxTSN", ylab="delta GC", pch=1,
+     xlim=c(-d,d))
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], g9_gc_e[[2]]-g9_gc_e[[3]], type="o", col="gray", pch=1)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="light blue", pch=1)
+
+
+g9_gc_e = LV_output[[1]]
+m_gc_e = LV_output[[2]]
+s_gc_e = LV_output[[3]]
+
+
+#abline(h=0)
+#abline(v=0)
+
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], g9_gc_e[[2]]-g9_gc_e[[3]], type="o", col="gray", pch=6)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="light blue", pch=6)
+
+
+
+
+a_g9_gc_e = ((BN_output[[1]][[2]]- BN_output[[1]][[3]]) + (LV_output[[1]][[2]]- LV_output[[1]][[3]]))/2
+a_m_gc_e =((BN_output[[2]][[2]]- BN_output[[2]][[3]]) + (LV_output[[2]][[2]]- LV_output[[2]][[3]]))/2
+a_s_gc_e = ((BN_output[[3]][[2]]- BN_output[[3]][[3]]) + (LV_output[[3]][[2]]- LV_output[[3]][[3]]))/2
+
+points(g9_gc_e[[1]][1:length(g9_gc_e[[2]])], a_g9_gc_e, type="o", col="black", pch=19)
+#points(m_gc_e[[1]][1:length(m_gc_e[[2]])], m_gc_e[[2]]-m_gc_e[[3]], type="o", col="blue", pch=19)
+
+points(m_gc_e[[1]][1:length(m_gc_e[[2]])], a_m_gc_e, type="o", col="blue", pch=19)
+
+legend("bottomleft", legend=c("Multiple Mean", "Multiple Brain", "Multiple Liver", "NS Mean", "NS Brain", "NS Liver"),
+       col=c("blue","light blue","light blue", "black","gray","gray"), bty = "n", lty=1, pch=c(19,1,6,19,1,6))
+
+dev.off()
+
+
+
