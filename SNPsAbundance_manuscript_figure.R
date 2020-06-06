15918dd325f0d1cd202c0373259b56f9417cbbb6
shaopei <sc2457@cornell.edu>
2020-06-06 10:05:07 +0800

    Fishers excat test for enrichemnt of As.TSS containing SNPs

--- SNPsAbundance_manuscript_figure.R
+++ SNPsAbundance_manuscript_figure.R
@@ -180,7 +180,7 @@ SNPsAbundanceAroundMaxTSNInTSS <-function(d=50, step=1,times=1, use.log=FALSE, u
     }
     
   }
-  return (list(x,s_plot, m_plot, dim(s), dim(m)))
+  return (list(x,s_plot, m_plot, dim(s), dim(m), b, dim(g9)))
   
 }
 
@@ -189,7 +189,7 @@ s_count <- NULL
 m_count <- NULL
 for(t in Organ){
   #SNPsAbundanceAroundMaxTSNInTSS(maxTSN, d=50, step=1,times=1, use.log=FALSE, use.sum=FALSE, name="BN", OnlyAsTSS= FALSE)
-  temp = SNPsAbundanceAroundMaxTSNInTSS(maxTSN, d=202, step=5,times=1, use.log=FALSE, use.sum=FALSE, name=t, OnlyAsTSS= TRUE)
+  temp = SNPsAbundanceAroundMaxTSNInTSS(d=202, step=5,times=1, use.log=FALSE, use.sum=FALSE, name=t, OnlyAsTSS= TRUE)
   s_count = c(s_count, temp[[4]][1])
   m_count = c(m_count, temp [[5]][1])
 
@@ -231,5 +231,202 @@ legend("topright",
        , bty = "n"
 )
 
+# SNPs fisher's exact test
+presentByBin <- function(x, bin=5){
+  # the last bin will be removed
+  output_bin<- NULL
+  for (i in (seq(1,length(x)-bin,bin))){
+    output_bin = c(output_bin, sum(sum(x[i:(i+bin-1)])>0)  )
+  }
+  return (output_bin)
+}
+
+TSSwithSNPsAroundMaxTSN <-function(d=50, bin=5,times=1, use.log=FALSE, use.sum=FALSE, name="", OnlyAsTSS= TRUE, OnlynonAsTSSwithAsMaxTSN= FALSE, p_value_cut=NULL , draw_plot=TRUE){ 
+  #d=202
+  step=1
+  AllTSS_maxTSN = read.table(paste(name,"_allReads_TSS_5mat5pat_uniq_pValue_maxTSNs.bed",sep=""), header = F)
+  getcolnames(AllTSS_maxTSN)[7:10]=c("masked_p_value","masked_fdr", "unmasked_p_value","unmasked_fdr")
+  colnames(AllTSS_maxTSN)[11:13]=c("chrm","chrmStart", "chrmEnd")
+  # TSS not allelic different (fdr>0.9)
+  g1 =  AllTSS_maxTSN[AllTSS_maxTSN$unmasked_fdr <= 0.1,c(11:16,9)]
+  g9=  AllTSS_maxTSN[AllTSS_maxTSN$unmasked_fdr > 0.9,c(11:16,9)]
+  dim(g1); max(g1$unmasked_p_value); min(g1$unmasked_p_value)
+  dim(g9); max(g9$unmasked_p_value); min(g9$unmasked_p_value)
+  
+  g1$chrmStart = g1$chrmStart-d
+  g1$chrmEnd = g1$chrmEnd+d
+  g9$chrmStart = g9$chrmStart-d
+  g9$chrmEnd = g9$chrmEnd+d
+  
+  # get the sum of SNPs in each bin (size = step)
+  g1.SNPs.temp <- read_read_mat_SNPs (SNP.bw, g1[,c(1:6)], step, times=times, use.log=use.log)
+  g9.SNPs.temp <- read_read_mat_SNPs (SNP.bw, g9[,c(1:6)], step, times=times, use.log=use.log)
+  
+  g9.SNPs <- t(apply(g9.SNPs.temp, 1, presentByBin, bin=bin))
+  
+  #AsTSS_maxTSN=read.table(file = paste(name,"_allReads_TSS_5mat5pat_uniq_WithAsMaxTSNunionAsTSSfdr0.1_maskedVSunmasked_pValue_maxTSNs.bed",sep=""), header = F)
+  AsTSS_maxTSN=read.table(file = paste(name,"_allReads_TSS_5mat5pat_uniq_maskedVSunmasked_pValue_maxTSNs.bed",sep=""), header = F)
+  
+  colnames(AsTSS_maxTSN)[7:10]=c("masked_p_value","masked_fdr", "unmasked_p_value","unmasked_fdr")
+  colnames(AsTSS_maxTSN)[11:13]=c("chrm","chrmStart", "chrmEnd")
+  
+  if (OnlyAsTSS){  # only use as.TSS. Remove TSS with as.maxTSN
+    AsTSS_maxTSN=AsTSS_maxTSN[AsTSS_maxTSN$unmasked_fdr <= 0.1 ,]
+  } else if (OnlynonAsTSSwithAsMaxTSN){  # only nonAS.TSS with as.maxTSN
+    AsTSS_maxTSN=AsTSS_maxTSN[AsTSS_maxTSN$unmasked_fdr > 0.1 ,]
+  }
+  if (is.null(p_value_cut)){
+    p_value_cut = max(AsTSS_maxTSN$unmasked_p_value[AsTSS_maxTSN$unmasked_fdr<=0.1])
+  }
+  
+  # asTSS driven by multiple base
+  m = AsTSS_maxTSN[AsTSS_maxTSN$masked_p_value <= p_value_cut,c(11:16,7)]
+  # asTSS driven by single base
+  s = AsTSS_maxTSN[AsTSS_maxTSN$masked_p_value > p_value_cut,c(11:16,7)]
+  
+  # if (dim(s)[1] > dim(m)[1]){
+  # s = s[order(s$masked_p_value, decreasing = TRUE),][1:dim(m)[1],c(11:16,7)]
+  # }else{
+  #   s = s[,c(11:16,7)]
+  # }
+  
+  cat(name, "\n")
+  cat ("dim(m), max(m$masked_p_value), min(m$masked_p_value)", "\n")
+  cat (dim(m), max(m$masked_p_value), min(m$masked_p_value), "\n")
+  cat("  dim(s); max(s$masked_p_value); min(s$masked_p_value)", "\n")
+  cat(dim(s), max(s$masked_p_value), min(s$masked_p_value), "\n")
+  
+  
+  s$chrmStart = s$chrmStart-d
+  s$chrmEnd = s$chrmEnd+d
+  m$chrmStart = m$chrmStart-d
+  m$chrmEnd = m$chrmEnd+d
+  
+  # get the sum of SNPs in each bin (size = step)
+  s.SNPs.temp <- read_read_mat_SNPs (SNP.bw, s[,c(1:6)], step, times=times, use.log=use.log)
+  m.SNPs.temp <- read_read_mat_SNPs (SNP.bw, m[,c(1:6)], step, times=times, use.log=use.log)
+  
+  s.SNPs <- t(apply(s.SNPs.temp, 1, presentByBin, bin=bin))
+  m.SNPs <- t(apply(m.SNPs.temp, 1, presentByBin, bin=bin))
+
+  step=bin
+  show.window <- d
+  if (use.sum){
+    a = colSums(g1.SNPs)
+    b = colSums(g9.SNPs)
+    s_plot = colSums(s.SNPs)
+    m_plot = colSums(m.SNPs)
+  }else{
+    a = colMeans(g1.SNPs)
+    b = colMeans(g9.SNPs)
+    s_plot = colMeans(s.SNPs)
+    m_plot = colMeans(m.SNPs)
+  }
+  
+  if (step >1){
+    x <- seq(-1*(show.window)+step/2, show.window, step)
+  }else{
+    x <- seq(-1*(show.window), show.window, step)
+  }
+  
+  x=x[1:length(b)]
+  
+  if(draw_plot){
+    pdf(paste(name,"_AsTSS_SNPsPresense_d=",d,"_step=",step,"-TSSwithSNPsAroundMaxTSN.pdf",sep=""), width=9.23, height = 6.52)
+    par(mfcol=c(2,1))
+    par(mar=c(6.1, 7.1, 2.1, 2.1)) #d l u r 5.1, 4.1, 4.1, 2.1
+    par(mgp=c(3,1,0))
+    par(cex.lab=2.2, cex.axis=2.2)
+    #par(cex=1.5)
+    pch_u=15
+    plot(x, b,col="black",  xlab="distance to maxTSN", ylab="TSS with SNPs", 
+         main=name, type="o", ylim=c(0,max(a,b,s_plot,m_plot)), pch=pch_u, las=1, frame=FALSE)
+    points(x, s_plot, col="red", type="o", pch=pch_u)
+    
+    legend("topleft", legend=c(paste("Single, n=", dim(s)[1],sep=""), paste("NS, n=", dim(g9)[1])),
+           col=c("red", "black"), bty = "n", lty=1, pch=pch_u)
+    
+    plot(x, m_plot, col="blue", xlab="distance to maxTSN", ylab="TSS with SNPs", main="", type="o", 
+         ylim=c(0,max(a,b,s_plot,m_plot)), las=1, pch=pch_u, frame=FALSE)
+    points(x, b,col="black", type="o", pch=pch_u)
+    
+    legend("topleft", legend=c(paste("Multiple, n=", dim(m)[1],sep=""), paste("NS, n=", dim(g9)[1])),
+           col=c("blue", "black"), bty = "n", lty=1, pch=pch_u) #, cex=2)
+    
+    dev.off()
+   }
+  return (list(x,s_plot, m_plot, dim(s), dim(m), b, dim(g9)))
+  
+}
 
 
+Organ=c("BN","LV","HT", "SP", "GI", "ST")
+g9_SNP_sum_count <- NULL
+s_SNP_sum_count <- NULL
+m_SNP_sum_count <- NULL
+#for(t in Organ){
+
+for (t in c("BN", "LV")){
+  bin=5
+  temp = TSSwithSNPsAroundMaxTSN(maxTSN, d=202, bin=bin,times=1, use.log=FALSE, use.sum=TRUE, name=t, OnlyAsTSS= TRUE)
+  # return (list(x,s_plot, m_plot, dim(s), dim(m), b, dim(g9)))   
+  s_TSSCountWithSNPs = temp[[2]]
+  m_TSSCountWithSNPs =temp [[3]]
+  g9_TSSCountWithSNPs = temp [[6]]
+  len_s=temp[[4]][1]
+  len_m=temp[[5]][1]
+  len_g9=temp[[7]][1]
+  
+  p.value <- NULL
+  odds.ratio <- NULL
+  testors <- NULL
+  for (i in (1:length(g9_TSSCountWithSNPs))){
+    
+    testor <-  matrix(c(s_TSSCountWithSNPs[i], len_s-s_TSSCountWithSNPs[i],
+                        g9_TSSCountWithSNPs[i], len_g9-g9_TSSCountWithSNPs[i]),
+                      nrow = 2,
+                      dimnames = list(TSS = c("With SNPs", "Without"),
+                                      KS.Test = c("Single", "NS"))); testor
+    
+    f = fisher.test(testor); f
+    p.value= c(p.value, f$p.value)
+    odds.ratio = c(odds.ratio, f$estimate)
+  }
+  
+  #par(mar=c(6.1, 7.1, 2.1, 2.1)) #d l u r 5.1, 4.1, 4.1, 2.1
+  #par(mgp=c(3,1,0))
+  #par(cex.lab=2.2, cex.axis=2.2)
+  pdf(paste(t,"_AsTSS_SNPsPresense_d=",d,"_bin=",bin,".pdf",sep=""), width=5.3, height = 7.5)
+  par(mfrow=c(4,1))
+  adjust.p = p.adjust(p.value, method="fdr")
+  plot(temp[[1]],-1*log10(adjust.p), type="o" , xlab="dist to maxTSN", main=paste(t,"_SingleBaseTSS_SNPs_bin=",bin, sep=""), las=1, col="red")
+  abline(h=-1*log10(0.05),col="gray")
+  #plot(temp[[1]],-1*log10(adjust.p), type="o" , xlab="dist to maxTSN", main=paste(t,"_SingleBaseTSS_SNPs", sep=""))
+  plot(temp[[1]],odds.ratio, type="o" , xlab="dist to maxTSN", main=paste(t,"_SingleBaseTSS_SNPs", sep=""), las=1)
+  abline(h=1, col="gray")
+  
+  
+  p.value <- NULL
+  odds.ratio <- NULL
+  testors <- NULL
+  for (i in (1:length(g9_TSSCountWithSNPs))){
+    
+    testor <-  matrix(c(m_TSSCountWithSNPs[i], len_m-m_TSSCountWithSNPs[i],
+                        g9_TSSCountWithSNPs[i], len_g9-g9_TSSCountWithSNPs[i]),
+                      nrow = 2,
+                      dimnames = list(TSS = c("With SNPs", "Without"),
+                                      KS.Test = c("Single", "NS"))); testor
+    
+    f = fisher.test(testor, alternative = "greater" ); f
+    p.value= c(p.value, f$p.value)
+    odds.ratio = c(odds.ratio, f$estimate)
+  }
+  adjust.p = p.adjust(p.value, method="fdr")
+  plot(temp[[1]],-1*log10(adjust.p), type="o" , xlab="dist to maxTSN", main=paste(t,"_multipleBaseTSS_SNPs_bin=",bin, sep="")
+       , las=1, col="blue")
+  abline(h=-1*log10(0.05),col="gray")
+  #plot(temp[[1]],-1*log10(adjust.p), type="o" , xlab="dist to maxTSN", main=paste(t,"_multipleBaseTSS_SNPs", sep=""))
+  plot(temp[[1]],odds.ratio, type="o" , xlab="dist to maxTSN", main=paste(t,"_multipleBaseTSS_SNPs", sep=""), las=1)
+  abline(h=1, col="gray")
+  dev.off()
+}
