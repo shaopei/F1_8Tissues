58c219a7b96d314fbbba79a6ca80988441e7e51d
shaopei <sc2457@cornell.edu>
2021-08-19 10:51:53 +0800

    Do TUs that are adjacent to a significant change tend to share the same change

---
+++ domains_cluster_more_than_chance_or_not.R
@@ -0,0 +1,147 @@
+setwd("~/Box Sync/Danko_lab_work/F1_8Tissues/transcription_level_analysis/domains_cluster_more_than_chance_or_not_tunit_protein/")
+
+df_0.1=read.table("BN_TunitProteinSrainEffect_binomtest_fdr0.1_adjacentTunit.bed")
+df_0.9=read.table("BN_TunitProteinSrainEffect_binomtest_fdr0.9_adjacentTunit.bed")
+
+colnames(df_0.1)[22]="pair_fdr"
+colnames(df_0.1)[23]="pair_distance"
+df_0.1$TunitLength = df_0.1$V3 - df_0.1$V2
+df_0.1$expLevel = df_0.1$V7 + df_0.1$V8 + df_0.1$V9
+
+colnames(df_0.9)[22]="pair_fdr"
+colnames(df_0.9)[23]="pair_distance"
+df_0.9$TunitLength = df_0.9$V3 - df_0.9$V2
+df_0.9$expLevel = df_0.9$V7 + df_0.9$V8 + df_0.9$V9
+
+
+#Do TUs that are adjacent to a significant change tend to share the same change?
+fisher.test(matrix(c(dim(df_0.9)[1],sum(df_0.9$pair_fdr <= 0.1), 
+                    dim(df_0.1)[1],sum(df_0.1$pair_fdr <= 0.1)
+                     ) , 2,2))
+
+
+# Do TUs change in the same direction of change? Or do they often have the opposite direction?
+df = df_0.1[df_0.1$pair_fdr <= 0.1,]
+temp <- data.frame(do.call(rbind, strsplit(as.character(df$V4), ",")))
+df$Tunit1_winP = temp[,1]
+temp <- data.frame(do.call(rbind, strsplit(as.character(df$V15), ",")))
+df$Tunit2_winP = temp[,1]
+
+# same direction
+sum(df$Tunit1_winP == df$Tunit2_winP) / dim(df)[1]
+# opposite direction
+sum(df$Tunit1_winP != df$Tunit2_winP) / dim(df)[1]
+pie(c(sum(df$Tunit1_winP == df$Tunit2_winP), sum(df$Tunit1_winP != df$Tunit2_winP) ),
+    label=c("same direction", "opposite direction"))
+
+# Distance distribution
+h1<- hist(log10(df_0.1$pair_distance[df_0.1$pair_fdr <= 0.1]))
+h1$counts=h1$counts/sum(h1$counts)
+plot(h1,col="red" 
+     ,density=25     
+     ,ylab="Proportion"
+     , xlim=c(0,7)
+     ,xlab="distance between pair Tunits"     
+     ,las=2
+     ,xaxt='n',main= ""
+     )
+axis(1, at=seq(0,7,1), labels=c(0,10,100,1000,"10,000","100,000","1000,000","10,000,000"), las=2)
+
+h2<- hist(log10(df_0.9$pair_distance[df_0.9$pair_fdr <= 0.1])
+          ,plot =FALSE
+)
+h2$counts=h2$counts/sum(h2$counts)
+plot(h2,col="blue" , add=T)
+plot(h1,col="red" ,density=25, add=T)
+
+legend("topright", 
+       legend = c( "FDR <=0.1","FDR > 0.9"), 
+       #pch=c(15,15),
+       cex=2, 
+       lty=c(0,0),
+       #bty="n",
+       lwd=1.5, 
+       density=c(25, 10000),
+       angle=c(45, 180),
+       #angle=45,
+       fill=c("red","blue")
+       , bty = "n"
+)
+
+ks.test(df_0.9$pair_distance[df_0.9$pair_fdr <= 0.1], 
+        df_0.1$pair_distance[df_0.1$pair_fdr <= 0.1] )
+
+
+# Tunit length
+h1<- hist(log10(df_0.1$TunitLength)[!duplicated(df_0.9[,1:6])])
+h1$counts=h1$counts/sum(h1$counts)
+plot(h1,col="red" 
+     ,density=25     
+     ,ylab="Proportion"
+     , xlim=c(0,7)
+     ,xlab="Tunit legnth"     
+     ,las=2
+     ,xaxt='n',main= ""
+)
+axis(1, at=seq(0,7,1), labels=c(0,10,100,1000,"10,000","100,000","1000,000","10,000,000"), las=2)
+
+h2<- hist(log10(df_0.9$TunitLength)[!duplicated(df_0.1[,1:6])]
+          ,plot =FALSE
+)
+h2$counts=h2$counts/sum(h2$counts)
+plot(h2,col="blue" , add=T)
+plot(h1,col="red" ,density=25, add=T)
+
+legend("topright", 
+       legend = c( "FDR <=0.1","FDR > 0.9"), 
+       #pch=c(15,15),
+       cex=2, 
+       lty=c(0,0),
+       #bty="n",
+       lwd=1.5, 
+       density=c(25, 10000),
+       angle=c(45, 180),
+       #angle=45,
+       fill=c("red","blue")
+       , bty = "n"
+)
+
+ks.test(df_0.9$TunitLength[!duplicated(df_0.9[,1:6])], 
+        df_0.1$TunitLength[!duplicated(df_0.1[,1:6])])
+
+# Tunit expression level
+h1<- hist(log10(df_0.1$expLevel)[!duplicated(df_0.9[,1:6])])
+h1$counts=h1$counts/sum(h1$counts)
+plot(h1,col="red" 
+     ,density=25     
+     ,ylab="Proportion"
+     , xlim=c(0,7)
+     ,xlab="Tunit Expression Level"     
+     ,las=2
+     ,xaxt='n',main= ""
+)
+axis(1, at=seq(0,7,1), labels=c(0,10,100,1000,"10,000","100,000","1000,000","10,000,000"), las=2)
+
+h2<- hist(log10(df_0.9$expLevel)[!duplicated(df_0.1[,1:6])]
+          ,plot =FALSE
+)
+h2$counts=h2$counts/sum(h2$counts)
+plot(h2,col="blue" , add=T)
+plot(h1,col="red" ,density=25, add=T)
+
+legend("topright", 
+       legend = c( "FDR <=0.1","FDR > 0.9"), 
+       #pch=c(15,15),
+       cex=2, 
+       lty=c(0,0),
+       #bty="n",
+       lwd=1.5, 
+       density=c(25, 10000),
+       angle=c(45, 180),
+       #angle=45,
+       fill=c("red","blue")
+       , bty = "n"
+)
+
+ks.test(df_0.9$expLevel[!duplicated(df_0.9[,1:6])], 
+        df_0.1$expLevel[!duplicated(df_0.1[,1:6])])
--- domains_cluster_more_than_chance_or_not.sh
+++ domains_cluster_more_than_chance_or_not.sh
@@ -4,24 +4,12 @@
 
 
 ## two group of TU
-cd /workdir/sc2457/F1_Tissues/transcription_level_analysis/domains_cluster_more_than_chance_or_not
-
-# use all tunits, not just tunits overlap with protein
-ln -s /workdir/sc2457/F1_Tissues/bigWig/tunit .  
-#ln -s /workdir/sc2457/F1_Tissues/PolyA_Allele-specific-manuscript/tunit_protein_coding .
+cd /workdir/sc2457/F1_Tissues/transcription_level_analysis/domains_cluster_more_than_chance_or_not_tunit_protein
+# use tunits overlap with protein
+#ln -s /workdir/sc2457/F1_Tissues/bigWig/tunit .  
+ln -s /workdir/sc2457/F1_Tissues/PolyA_Allele-specific-manuscript/tunit_protein_coding .
 
 ln -s  /workdir/sc2457/F1_Tissues/map2ref_1bpbed_map5_MultiBaseRunOn/map2ref_1bpbed_map5 .
-
-# seperate tunit ptredicts into plus and minus strand
-for f in tunit/*.preds.full.bed
-do 
-PREFIX=`echo $f|rev|cut -d . -f 4- |rev|cut -d / -f 2`
-echo $PREFIX
-grep plus $f > ${PREFIX}_plus.bed &
-grep minus $f > ${PREFIX}_minus.bed &
-done
-
-#*/
 
 ### calculate allele specifcity within each tunit predicts
 PL=/workdir/sc2457/alleleDB/alleledb_pipeline_mouse
@@ -36,14 +24,44 @@ BinomialTest(){
   MAT_READ_BED=$3
   PAT_READ_BED=$4
 
-  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${MAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3, $1,$2,$3,$7,$8,$9,$10}' > ${j}.mat_cov.bed &
-  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${PAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3, $1,$2,$3,$7,$8,$9,$10}' > ${j}.pat_cov.bed &
+  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${MAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3t$6, $1,$2,$3,$7,$8,$9,$10, $6}' > ${j}.mat_cov.bed &
+  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${PAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3t$6, $1,$2,$3,$7,$8,$9,$10, $6}' > ${j}.pat_cov.bed &
   #bedtools coverage -s -a $f -b ${IDENTICAL_READ_BED} -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3, $1,$2,$3,$7,$8,$9,$10}' |LC_ALL=C sort -k1,1V -k2,2n > ${j}.iden_cov.bed &
   wait
 
   # filter the block and only keep block with at lease 1 allele-specific read
-  join -t $'\t' -j 1 -o 1.1,1.2,1.3,1.4,1.5,2.5 ${j}.mat_cov.bed ${j}.pat_cov.bed | awk 'BEGIN{OFS="\t"} ($5+$6 >0) {print $0}' | \
-  awk 'BEGIN{OFS="\t"; t=","} ($5>$6) {print $2, $3, $4, "M"t$5t$6, $5, $6, "-" }  ($5<$6) {print $2, $3, $4, "P"t$5t$6, $5, $6, "-"}  ($5==$6) {print $2, $3, $4, "S"t$5t$6, $5, $6, "-"} '  \
+  join -t $'\t' -j 1 -o 1.1,1.2,1.3,1.4,1.5,2.5,1.9 ${j}.mat_cov.bed ${j}.pat_cov.bed | awk 'BEGIN{OFS="\t"} ($5+$6 >0) {print $0}' | \
+  awk 'BEGIN{OFS="\t"; t=","} ($5>$6) {print $2, $3, $4, "M"t$5t$6, $5, $6, "-", $7 }  ($5<$6) {print $2, $3, $4, "P"t$5t$6, $5, $6, "-", $7 }  ($5==$6) {print $2, $3, $4, "S"t$5t$6, $5, $6, "-", $7} '  \
+  | LC_ALL=C sort -k1,1V -k2,2n --parallel=30 > ${j}.merged_cov.bed
+
+  mv ${j}.mat_cov.bed ${j}.pat_cov.bed toremove
+  # output of BinomialTestFor_merged_cov.bed.py:(hmm+BinomialTest) if p-value <= 0.05, remain what it got from hmm (can ne M,P, or S), otherwise S.
+  python2 ${PL}/BinomialTestFor_merged_cov.bed.py ${j}.merged_cov.bed ${j}_binomtest.bed
+  mv ${j}.merged_cov.bed toremove
+  R --vanilla --slave --args $(pwd) ${j}_binomtest.bed ${j}_binomtest_fdr.bed < getFDR.R
+  # python2 ${PL}/FalsePosFor_merged_cov.bed.py ${j}_binomtest.bed ${FDR_SIMS} ${FDR_CUTOFF} > ${j}_binomtest_FDR${FDR_CUTOFF}.txt &
+  #  awk 'NR==1 { print $0 } NR>1 && $9 <= thresh { print $0 }'  thresh=$(awk 'END {print $6}' ${j}_binomtest_FDR${FDR_CUTOFF}.txt) < ${j}_binomtest.bed  > ${j}_interestingHets.bed
+}
+
+BinomialTest_IDE(){
+  # BinomialTest $f $j $MAT_READ_BED $PAT_READ_BED
+  # Need to seperate plus and minus strand before using this function. This function ignore strandness in the output
+  f=$1  #bed file containing region to perform test
+  j=$2  # prefix for output
+  MAT_READ_BED=$3
+  PAT_READ_BED=$4
+  IDE_READ_BED=$5
+
+  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${MAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3t$6, $1,$2,$3,$7,$8,$9,$10, $6}' > ${j}.mat_cov.bed &
+  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${PAT_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3t$6, $1,$2,$3,$7,$8,$9,$10, $6}' > ${j}.pat_cov.bed &
+  bedtools coverage -s -a <(cat $f | LC_ALL=C sort -k1,1V -k2,2n --parallel=30| awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5,$6}') -b <(zcat ${IDE_READ_BED} | grep -v chrX |grep -v chrY | grep -v chrMT) -sorted| awk 'BEGIN {OFS="\t"; t="_"} {print $1t$2t$3t$6, $1,$2,$3,$7,$8,$9,$10, $6}' > ${j}.ide_cov.bed &
+  wait
+
+  # filter the block and only keep block with at lease 1 allele-specific read
+  join -t $'\t' -j 1 -o 1.1,1.2,1.3,1.4,1.5,2.5,1.9 ${j}.mat_cov.bed ${j}.pat_cov.bed > ${j}.merged_cov.temp
+  join -t $'\t' -j 1 -o 1.1,1.2,1.3,1.4,1.5,1.6,2.5,1.7 ${j}.merged_cov.temp ${j}.ide_cov.bed \
+  | awk 'BEGIN{OFS="\t"} ($5+$6 >0) {print $0}' | \
+  awk 'BEGIN{OFS="\t"; t=","} ($5>$6) {print $2, $3, $4, "M"t$5t$6, $5, $6, $7, $8 }  ($5<$6) {print $2, $3, $4, "P"t$5t$6, $5, $6, $7, $8 }  ($5==$6) {print $2, $3, $4, "S"t$5t$6, $5, $6, $7, $8 } '  \
   | LC_ALL=C sort -k1,1V -k2,2n --parallel=30 > ${j}.merged_cov.bed
 
   mv ${j}.mat_cov.bed ${j}.pat_cov.bed toremove
@@ -71,21 +89,79 @@ for Head in BN LV #HT SK SP LV GI ST KD
             PAT_READ_BED=${PREFIX}.pat.bowtie.gz_AMBremoved_sorted_specific.map2ref.map5.1bp.sorted.bed.gz
             #IDENTICAL_READ_BED=${PREFIX}.mat.bowtie.gz_AMBremoved_sorted_identical.map2ref.sorted.bed.gz
             P=${Head}_${cross}_all_h5
-            ln -s ${Head}_all_h5_plus.bed ${P}_plus.bed
-            ln -s ${Head}_all_h5_minus.bed ${P}_minus.bed
+            ln -s tunit_protein_coding/${Head}_all_h5.preds.full_inProtein_coding.bed ${P}.bed
 
             if [[ "$cross" == "MB6" ]] ; then
-                BinomialTest ${P}_plus.bed ${P}_plus ${MAT_READ_BED} ${PAT_READ_BED} &
-                BinomialTest ${P}_minus.bed ${P}_minus ${MAT_READ_BED} ${PAT_READ_BED} &
+                BinomialTest ${P}.bed ${P} ${MAT_READ_BED} ${PAT_READ_BED} &
             elif [[ "$cross" == "PB6" ]]  ; then
             # switch -m and -p for PB6
-                BinomialTest ${P}_plus.bed ${P}_plus ${PAT_READ_BED} ${MAT_READ_BED} &
-                BinomialTest ${P}_minus.bed ${P}_minus ${PAT_READ_BED} ${MAT_READ_BED} &
+                BinomialTest ${P}.bed ${P} ${PAT_READ_BED} ${MAT_READ_BED} &
             fi
     done
 done
     wait
 
+# identify imprinted tunits, remove those. do binomial test again witj pool reads.
+for Head in BN LV 
+    do 
+intersectBed -wb -a ${Head}_MB6_all_h5_binomtest_fdr.bed -b ${Head}_PB6_all_h5_binomtest_fdr.bed | \
+awk 'BEGIN {OFS="\t"} ($2==$13 && $3==$14 && substr($4,1,1) == substr($15,1,1) && $11+0 <=0.1 && $22+0 <= 0.1 ){print $1,$2,$3,$4,$5,$10}' \
+> ${Head}_Tunit_imprinted_effect_fdr0.1.bed
+done
+
+for Head in BN LV 
+    do intersectBed -v -a tunit_protein_coding/${Head}_all_h5.preds.full_inProtein_coding.bed \
+    -b ${Head}_Tunit_imprinted_effect_fdr0.1.bed > ${Head}_all_h5.preds.full_inProtein_coding_NoImprinted.bed
+done
+
+# group reads from MB6 and PB6
+bed_dir=map2ref_1bpbed_map5
+for Head in BN LV #HT SK SP LV GI ST KD
+    do 
+    zcat ${bed_dir}/${Head}_*_all_R1.mat.bowtie.gz_AMBremoved_sorted_specific.map2ref.map5.1bp.sorted.bed.gz \
+    |grep -v chrX |grep -v chrY | grep -v chrMT| LC_ALL=C sort -k1,1V -k2,2n --parallel=30 |gzip > ${Head}_MAT_READ_BED.temp.gz &
+    zcat ${bed_dir}/${Head}_*_all_R1.pat.bowtie.gz_AMBremoved_sorted_specific.map2ref.map5.1bp.sorted.bed.gz \
+    |grep -v chrX |grep -v chrY | grep -v chrMT| LC_ALL=C sort -k1,1V -k2,2n --parallel=30 |gzip > ${Head}_PAT_READ_BED.temp.gz &
+    zcat ${bed_dir}/${Head}_*_all_R1.mat.bowtie.gz_AMBremoved_sorted_identical.map2ref.map5.1bp.sorted.bed.gz \
+    |grep -v chrX |grep -v chrY | grep -v chrMT| LC_ALL=C sort -k1,1V -k2,2n --parallel=30 |gzip > ${Head}_IDE_READ_BED.temp.gz &
+
+done
+
+
+for Head in BN LV #HT SK SP LV GI ST KD
+    do 
+    echo $Head #BN
+    P=${Head}_TunitProteinSrainEffect
+    ln -s ${Head}_all_h5.preds.full_inProtein_coding_NoImprinted.bed ${P}.bed
+    BinomialTest_IDE ${P}.bed ${P} ${Head}_MAT_READ_BED.temp.gz ${Head}_PAT_READ_BED.temp.gz ${Head}_IDE_READ_BED.temp.gz &
+done
+wait
+
+
+# bias fdr<=0.1, control fdr > 0.9
+
+for Head in BN LV 
+    do 
+    	cat ${Head}_TunitProteinSrainEffect_binomtest_fdr.bed | awk 'BEGIN {OFS="\t"} ($11+0 <= 0.1) {print $1,$2,$3,$4,$5, $10, $6,$7,$8,$9, $11}' > ${Head}_TunitProteinSrainEffect_binomtest_fdr0.1.bed
+    	cat ${Head}_TunitProteinSrainEffect_binomtest_fdr.bed | awk 'BEGIN {OFS="\t"} ($11+0 > 0.9) {print $1,$2,$3,$4,$5, $10, $6,$7,$8,$9, $11}' > ${Head}_TunitProteinSrainEffect_binomtest_fdr0.9.bed
+        cat ${Head}_TunitProteinSrainEffect_binomtest_fdr.bed | awk 'BEGIN {OFS="\t"} {print $1,$2,$3,$4,$5, $10, $6,$7,$8,$9, $11}' > ${Head}_TunitProteinSrainEffect_binomtest_fdrAll.bed
+done
+
+
+#1.  identify tunits that overlap (in opposite strand)
+# -S	Require opposite strandedness
+for fdr in fdr0.1 fdr0.9
+do 
+bedtools closest -S -d -a ${Head}_TunitProteinSrainEffect_binomtest_${fdr}.bed -b ${Head}_TunitProteinSrainEffect_binomtest_fdrAll.bed \
+| awk 'BEGIN {OFS="\t"} ($23+0 ==0) {print $0}'  > ${Head}_TunitProteinSrainEffect_binomtest_${fdr}_adjacentTunit.bed
+#2. identify the closet one that do not overlap
+bedtools closest -io -d -a <(intersectBed -v -a ${Head}_TunitProteinSrainEffect_binomtest_${fdr}.bed -b ${Head}_TunitProteinSrainEffect_binomtest_${fdr}_adjacentTunit.bed) -b ${Head}_TunitProteinSrainEffect_binomtest_fdrAll.bed \
+>> ${Head}_TunitProteinSrainEffect_binomtest_${fdr}_adjacentTunit.bed
+done
+
+
+
+############
 
 # bias fdr<=0.1, control fdr > 0.9
 # biased, strain effect, tunits
