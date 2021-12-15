ca1d6ce9c21ed8196b71e912d4123b0fa703fe91
shaopei <sc2457@cornell.edu>
2021-12-15 18:02:01 -0500

    make map5 1bp for heatmap

--- F1_TSN_Generate_BigWig.sh
+++ F1_TSN_Generate_BigWig.sh
@@ -49,6 +49,43 @@ done
 done
 
 
+#make mat, pat bigwig for each sample for heatmap
+# BN
+cd /workdir/sc2457/F1_Tissues/second_batch/map2ref_1bpbed_map5
+ln -s ../allelicbias-PersonalGenome_P.CAST_M.B6-BN_*R1/BN_*_R1.mat.bowtie.gz_AMBremoved_sorted_specific.map2ref.sorted.bed.gz .
+ln -s ../allelicbias-PersonalGenome_P.CAST_M.B6-BN_*R1/BN_*_R1.pat.bowtie.gz_AMBremoved_sorted_specific.map2ref.sorted.bed.gz .
+
+export mouse_genome=/local/storage/data/short_read_index/mm10/bwa.rRNA-0.7.8-r455/mm10.rRNA.fa.gz
+export mouse_chinfo=/local/storage/data/mm10/mm10.chromInfo
+
+#for Head in  BN HT  SK  SP  KD  LV  GI  ST
+#do
+# Convert to bedGraph ... 
+Head=BN
+
+for s in  MB6_F MB6_G PB6_B PB6_C PB6_D PB6_E
+do 
+  for p in mat pat 
+  do 
+    bedtools genomecov -bg -i <(zcat ${Head}_${s}_R1.${p}.bowtie.gz_AMBremoved_sorted_specific.map2ref.sorted.bed.gz | awk '{OFS="\t"}($6=="+") {print $1, $2, $2+1, "o", $5, $6}; ($6=="-") {print $1, $3-1, $3, "o", $5, $6} '|LC_COLLATE=C sort -k 1,1) \
+    -g mouse_chinfo.txt -strand + |LC_COLLATE=C sort -k1,1 -k2,2n > ${Head}_${s}_R1.${p}.bowtie.gz_AMBremoved_sorted_specific.map2ref.map5.1bp.sorted_plus.bedGraph &
+    bedtools genomecov -bg -i <(zcat ${Head}_${s}_R1.${p}.bowtie.gz_AMBremoved_sorted_specific.map2ref.sorted.bed.gz | awk '{OFS="\t"}($6=="+") {print $1, $2, $2+1, "o", $5, $6}; ($6=="-") {print $1, $3-1, $3, "o", $5, $6} '|LC_COLLATE=C sort -k 1,1) \
+    -g mouse_chinfo.txt -strand - |LC_COLLATE=C sort -k1,1 -k2,2n > ${Head}_${s}_R1.${p}.bowtie.gz_AMBremoved_sorted_specific.map2ref.map5.1bp.sorted_minus.bedGraph &
+done
+wait
+
+for f in *bowtie.gz_AMBremoved_sorted_*.map2ref.sorted.bed.gz 
+do 
+h=`echo $f |rev |cut -d . -f 4-|rev`
+j=${h}.map5.1bp.sorted
+  cat $j\_minus.bedGraph | awk 'BEGIN{OFS="\t"} {print $1,$2,$3,-1*$4}' >  $j\_minus.inv.bedGraph 
+  bedGraphToBigWig $j\_minus.inv.bedGraph ${mouse_chinfo} $j\_minus.bw &
+  bedGraphToBigWig $j\_plus.bedGraph ${mouse_chinfo} $j\_plus.bw &
+done
+
+
+###
+
 # single base runon
 # make bigwig tracks from mat, pat, iden seperatley 
 for f in *bowtie.gz_AMBremoved_sorted_*.map2ref.map5.1bp.sorted.bed.gz
